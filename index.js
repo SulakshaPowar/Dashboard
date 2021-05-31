1778a86764f4f0651aca5f73de67436588997e25
SulakshaPowar <55498221+SulakshaPowar@users.noreply.github.com>
2021-05-31 10:53:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express= require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT||3000;
+app.use(express.json())
+const router = require("./router/router");
+app.use('/api', router);
+
+app.listen(PORT, () => console.log("server running on port"+ PORT));
